4498178c91658b281898fd7338ca825303de0d0e
Unity19207 <yajadhav19@gmail.com>
2024-09-03 17:14:22 +0530

    Second Version - Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,28 +11,28 @@ import { useDispatch } from "react-redux";
 import { authActions } from "./store";
 
 function App() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (sessionStorage.getItem("id")) {
-            dispatch(authActions.login());
-        }
-    }, [dispatch]);
-    return (
-        <>
-            <Router>
-                <div className="z-10 w-[100%] px-12 py-6 items-center  ">
-                    <Navbar />
-                </div>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/todo" element={<Todo />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/signin" element={<Signin />} />
-                </Routes>
-            </Router>
-        </>
-    );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (sessionStorage.getItem("id")) {
+      dispatch(authActions.login());
+    }
+  }, [dispatch]);
+  return (
+    <>
+      <Router>
+        <div className="z-10 w-[100%] px-12 py-6 items-center  ">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
